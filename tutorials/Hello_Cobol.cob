5f4cd507eb7e003fbb06a65040baf4550db60c5b
ignamiguel <iglesiasnacho@gmail.com>
2016-07-10 09:42:44 -0300

    Added submodules.

--- tutorials/Hello_Cobol.cob
+++ tutorials/Hello_Cobol.cob
@@ -36,6 +36,10 @@
         05 WS-HOUSE-NUMBER PIC 9(3).
         05 WS-COUNTRY PIC X(15).
         05 WS-PINCODE PIC 9(6) VALUE 123456.
+
+       01 WS-OLD-X PIC X(10) VALUE 'PEPE 1234'.
+       01 WS-NEW-9 REDEFINES WS-OLD-X PIC 9(8).
+       01 WS-NEW-A REDEFINES WS-OLD-X PIC A(10).
       *-----------------------
        PROCEDURE DIVISION.
       *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
@@ -43,27 +47,43 @@
       **
       * The main procedure of the program
       **
-            DISPLAY 'Hello World'.
+      *      PERFORM BASIC-PROCEDURE.
+      *      PERFORM USER-INPUT-PROCEDURE.
+      *      PERFORM REPLACING-PROCEDURE.
+      *      PERFORM REDEFINES-PROCEDURE.
+            PERFORM IF-EXAMPLE.
+
+            STOP RUN.
+      ** add other procedures here
+      *-----------------------------------------------------------*
+      *-----------------------------------------------------------*
+       BASIC-PROCEDURE.
+            DISPLAY '*---------------------------*'.
+            DISPLAY '-- START BASIC-PROCEDURE --'.
             MOVE 'TutorialsPoint' TO WS-NAME.
             DISPLAY "My name is : "WS-NAME.
             DISPLAY "My ID is : "WS-ID.
-
-            DISPLAY " ".
             DISPLAY "WS-NUM1 : "WS-NUM1.
             DISPLAY "WS-NUM2 : "WS-NUM2.
             DISPLAY "WS-NUM3 : "WS-NUM3.
             DISPLAY "WS-NEW-NAME : "WS-NEW-NAME.
             DISPLAY "WS-NEW-ID : "WS-NEW-ID.
-
-      * USER INPUT
-            DISPLAY " ".
+      *-----------------------------------------------------------*
+      *-----------------------------------------------------------*
+       USER-INPUT-PROCEDURE.
+       *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+            DISPLAY '*---------------------------*'.
+            DISPLAY '-- START USER-INPUT-PROCEDURE --'.
             DISPLAY "Enter student name and hit enter: "
-      *      ACCEPT WS-STUDENT-NAME.
+      *     ACCEPT WS-STUDENT-NAME.
             ACCEPT WS-DATE FROM DATE.
             DISPLAY "Name :  " WS-STUDENT-NAME.
-            DISPLAY "Date : " WS-DATE.
-
-            DISPLAY " ".
+            DISPLAY "System Date : " WS-DATE.
+      *-----------------------------------------------------------*
+      *-----------------------------------------------------------*
+       REPLACING-PROCEDURE.
+            DISPLAY '*---------------------------*'.
+            DISPLAY '-- START REPLACING-PROCEDURE --'.
             INITIALIZE WS-OTHER-NAME, WS-ADDRESS.
             INITIALIZE WS-OTHER-ID REPLACING NUMERIC DATA BY 12345.
             DISPLAY "My name is   : "WS-OTHER-NAME.
@@ -72,7 +92,26 @@
             DISPLAY "House Number : "WS-HOUSE-NUMBER.
             DISPLAY "Country      : "WS-COUNTRY.
             DISPLAY "Pincode      : "WS-PINCODE.
+      *-----------------------------------------------------------*
+      *-----------------------------------------------------------*
+       REDEFINES-PROCEDURE.
+            DISPLAY '*---------------------------*'.
+            DISPLAY '-- START REDEFINES-PROCEDURE --'.
+            DISPLAY "WS-OLD-X: " WS-OLD-X.
 
-            STOP RUN.
-      ** add other procedures here
+            MOVE 1234 TO WS-NEW-9.
+            DISPLAY "WS-NEW-9: " WS-NEW-9.
+
+            MOVE 'ABCD' TO WS-NEW-A.
+            DISPLAY "WS-NEW-A: " WS-NEW-A.
+
+            DISPLAY "WS-OLD-X: " WS-OLD-X.
+      *-----------------------------------------------------------*
+      *-----------------------------------------------------------*
+       IF-EXAMPLE.
+            IF 1 = 1 THEN
+               DISPLAY "1 = 1 OK"
+            ELSE
+               DISPLAY "1 NOT = 1"
+            END-IF.
        END PROGRAM COBOL_TUTORIALS.
