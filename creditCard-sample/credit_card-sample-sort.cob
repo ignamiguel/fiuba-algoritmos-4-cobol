89818622c8e6a9578a660122e7c7e82925baef3d
ignamiguel <iglesiasnacho@gmail.com>
2016-07-25 23:27:15 -0300

    Report in progress

--- creditCard-sample/credit_card-sample-sort.cob
+++ creditCard-sample/credit_card-sample-sort.cob
@@ -36,38 +36,38 @@
        FD Cupon1_file.
        01 Cupon1_Record.
          88 EOF-CUPON-1 VALUE HIGH-VALUE.
-        03 C1-NRO-TARJ            PIC 9(10).
-        03 C1-NRO-CUPON           PIC 9(5).
+        03 C1-NRO-TARJ                   PIC 9(10).
+        03 C1-NRO-CUPON                  PIC 9(5).
         03 C1-FECHA-COMPRA.
-          06 C1-FILLER            PIC X(2).
-          06 C1-DAY               PIC X(2).
-          06 C1-MONTH             PIC X(2).
-          06 C1-YEAR              PIC X(4).
-        03 C1-IMPORTE            PIC 9(6)V99.
+          06 C1-FILLER                   PIC X(2).
+          06 C1-DAY                      PIC X(2).
+          06 C1-MONTH                    PIC X(2).
+          06 C1-YEAR                     PIC X(4).
+        03 C1-IMPORTE                    PIC 9(6)V99.
 
        FD Cupon2_file.
        01 Cupon2_Record.
          88 EOF-CUPON-2 VALUE HIGH-VALUE.
-        03 C2-NRO-TARJ            PIC 9(10).
-        03 C2-NRO-CUPON           PIC 9(5).
+        03 C2-NRO-TARJ                   PIC 9(10).
+        03 C2-NRO-CUPON                  PIC 9(5).
         03 C2-FECHA-COMPRA.
-          06 C2-FILLER            PIC X(2).
-          06 C2-DAY               PIC X(2).
-          06 C2-MONTH             PIC X(2).
-          06 C2-YEAR              PIC X(4).
-        03 C2-IMPORTE            PIC 9(6)V99.
+          06 C2-FILLER                   PIC X(2).
+          06 C2-DAY                      PIC X(2).
+          06 C2-MONTH                    PIC X(2).
+          06 C2-YEAR                     PIC X(4).
+        03 C2-IMPORTE                    PIC 9(6)V99.
 
        FD Cupon3_file.
        01 Cupon3_Record.
          88 EOF-CUPON-3 VALUE HIGH-VALUE.
-        03 C3-NRO-TARJ            PIC 9(10).
-        03 C3-NRO-CUPON           PIC 9(5).
+        03 C3-NRO-TARJ                   PIC 9(10).
+        03 C3-NRO-CUPON                  PIC 9(5).
         03 C3-FECHA-COMPRA.
-          06 C3-FILLER            PIC X(2).
-          06 C3-DAY               PIC X(2).
-          06 C3-MONTH             PIC X(2).
-          06 C3-YEAR              PIC X(4).
-        03 C3-IMPORTE            PIC 9(6)V99.
+          06 C3-FILLER                   PIC X(2).
+          06 C3-DAY                      PIC X(2).
+          06 C3-MONTH                    PIC X(2).
+          06 C3-YEAR                     PIC X(4).
+        03 C3-IMPORTE                    PIC 9(6)V99.
 
        FD SaldoFile.
        01 SaldoRecord.
@@ -75,56 +75,56 @@
          02 SALD-KEY.
            04 SALD-NRO-TARJ            PIC 9(10).
            04 SALD-FECHA               PIC X(10).
-         02 SALD-IMPORTE             PIC 9(6)V99.
+         02 SALD-IMPORTE                 PIC 9(6)V99.
 
        SD WorkFile.
        01 SortRecord.
           88 EOF-WorkFile VALUE HIGH-VALUE.
-          02 sort-titular            PIC X(30).
-          02 sort-documento          PIC 9(11).
-          02 sort-nro-tarjeta        PIC 9(10).
-          *>02 sort-saldo             PIC Z(3),Z(2)9V99.
-          02 sort-saldo              PIC 9(6)V99.
-          02 sort-nro-cupon          PIC 9(5).
+          02 sort-titular                PIC X(30).
+          02 sort-documento              PIC 9(11).
+          02 sort-nro-tarjeta            PIC 9(10).
+          *>02 sort-saldo                PIC Z(3),Z(2)9V99.
+          02 sort-saldo                  PIC 9(6)V99.
+          02 sort-nro-cupon              PIC 9(5).
           02 sort-fecha.
-             06 sort-fecha-FILLER    PIC X(2).
-             06 sort-fecha-DAY       PIC X(2).
-             06 sort-fecha-MONTH     PIC X(2).
-             06 sort-fecha-YEAR      PIC X(4).
-          02 sort-importe            PIC 9(6)V99.
+             06 FILLER                   PIC X(2).
+             06 sort-fecha-DAY           PIC X(2).
+             06 sort-fecha-MONTH         PIC X(2).
+             06 sort-fecha-YEAR          PIC X(4).
+          02 sort-importe                PIC 9(6)V99.
 
        FD ReportFile.
-       01 ReportRecord               PIC X(60).
+       01 ReportRecord                   PIC X(60).
 
        WORKING-STORAGE SECTION.
        *> WS prefix stands for "working storage"
-       01   SaldoStatus              PIC X(2).
+       01   SaldoStatus                  PIC X(2).
            88 SaldoSuccess   VALUE "00".
            88 SaldoNotFound  VALUE "23".
 
-       01   TarjetaStatus            PIC X(2).
-       01   WS-CreditCardValid       PIC X(1).
+       01   TarjetaStatus                PIC X(2).
+       01   WS-CreditCardValid           PIC X(1).
           88 CC-VALID VALUE HIGH-VALUE.
           88 CC-INVALID VALUE LOW-VALUE.
-       01   WS-CC-Key                PIC 9(10).
-       01   WS-Saldo-amount          PIC 9(10)V99.
-       01   WS-total-amount          PIC 9(10)V99.
-       01   WS-cupon-counter         PIC 9(2).
+       01   WS-CC-Key                    PIC 9(10).
+       01   WS-Saldo-amount              PIC 9(10)V99.
+       01   WS-total-amount              PIC 9(10)V99.
+       01   WS-cupon-counter             PIC 9(2).
 
        01 Cupon_Record.
-        03 WS-nro-tarjeta            PIC 9(10).
-        03 WS-NRO-CUPON              PIC 9(5).
+        03 WS-nro-tarjeta                PIC 9(10).
+        03 WS-NRO-CUPON                  PIC 9(5).
         03 WS-FECHA-COMPRA.
-          06 WS-FILLER            PIC X(2).
-          06 WS-DAY               PIC X(2).
-          06 WS-MONTH             PIC X(2).
-          06 WS-YEAR              PIC X(4).
-        03 WS-IMPORTE          PIC 9(6)V99.
+          06 WS-FILLER                   PIC X(2).
+          06 WS-DAY                      PIC X(2).
+          06 WS-MONTH                    PIC X(2).
+          06 WS-YEAR                     PIC X(4).
+        03 WS-IMPORTE                    PIC 9(6)V99.
 
-       01 WS-C1-IMPORTE             PIC 9(6)V99.
+       01 WS-C1-IMPORTE                  PIC 9(6)V99.
 
        01 WS-TJ-KEY.
-         03 WS-TJ-NRO-TARJ              PIC 9(10).
+         03 WS-TJ-NRO-TARJ               PIC 9(10).
 
        01 WS-SALD-KEY.
           02 WS-SALD-NRO-TARJ            PIC 9(10).
@@ -134,8 +134,40 @@
          88 EOF-TARJETA VALUE HIGH-VALUE.
          02 TJ-KEY.
            03 TJ-NRO-TARJ        PIC 9(10).
-         02 TJ-TITULAR           PIC X(30).
-         02 TJ-DOCUMENTO         PIC 9(11).
+         02 TJ-TITULAR                   PIC X(30).
+         02 TJ-DOCUMENTO                 PIC 9(11).
+
+       01 ReportLine.
+          02 FILLER                      PIC X(58).
+          02 ReportPage                  PIC X(02).
+
+       01 Report_page_num                PIC 9(2).
+
+        01  WS-CURRENT-DATE-FIELDS.
+           05  WS-CURRENT-DATE.
+               10 WS-CURRENT-YEAR        PIC X(04).
+               10 WS-CURRENT-MONTH       PIC X(02).
+               10 WS-CURRENT-DAY         PIC X(02).
+           05  WS-CURRENT-TIME.
+               10 WS-CURRENT-HOUR        PIC  9(2).
+               10  WS-CURRENT-MINUTE     PIC  9(2).
+               10  WS-CURRENT-SECOND     PIC  9(2).
+               10  WS-CURRENT-MS         PIC  9(2).
+               10  WS-GMT-SIGN           PIC X(01).
+               10  WS-GMT-TIME           PIC X(04).
+
+       01 ReportSecondLine.
+         02 RSL_date_title               PIC X(7) VALUE "Fecha: ".
+         02 RSL_date_day                 PIC X(2).
+         02 FILLER                       PIC X(2) VALUE "/".
+         02 RSL_date_month               PIC X(2).
+         02 FILLER                       PIC X(2) VALUE "/".
+         02 RSL_date_year                PIC X(4).
+
+       01 Empty_line.
+           03 FILLER                     PIC X(60) VALUE SPACES.
+
+       01 ws-titular                     PIC X(30).
 
        PROCEDURE DIVISION.
        Begin.
@@ -155,14 +187,23 @@
 
        Output_Process SECTION.
          OPEN OUTPUT ReportFile.
+         *> Set Report_page_num to zero
+         INITIALIZE Report_page_num.
 
          PERFORM Get_record_from_sort_file.
 
+         PERFORM Print_header.
+
          PERFORM UNTIL EOF-WorkFile
 
+           PERFORM Print_first_section
+
+           *> Process all record for cc holder
            WRITE ReportRecord FROM SortRecord
 
            PERFORM Get_record_from_sort_file
+
+           ADD 1 TO Report_page_num
 
          END-PERFORM.
          CLOSE ReportFile.
@@ -170,6 +211,32 @@
 
        Get_record_from_sort_file.
          RETURN WorkFile AT END SET EOF-WorkFile TO TRUE.
+
+
+       Print_header.
+          *> To do...
+
+       Print_first_section.
+          MOVE
+       "Nro                       Hoja:                            X"
+          TO ReportLine.
+          MOVE Report_page_num TO ReportPage.
+
+
+          WRITE ReportRecord FROM ReportLine.
+
+          *> Write date
+          MOVE FUNCTION CURRENT-DATE TO WS-CURRENT-DATE-FIELDS.
+          MOVE WS-CURRENT-DAY TO RSL_date_day.
+          MOVE WS-CURRENT-MONTH TO RSL_date_month.
+          MOVE WS-CURRENT-YEAR TO RSL_date_year.
+
+          WRITE ReportRecord FROM ReportSecondLine.
+          INITIALIZE ReportLine.
+          MOVE "                       LISTA DE CUPONES"
+       TO ReportLine.
+
+       WRITE ReportRecord FROM ReportLine.
 
        Open_All_Files.
           OPEN INPUT SaldoFile.
